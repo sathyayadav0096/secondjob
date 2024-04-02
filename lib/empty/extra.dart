89a42de6c02c128410d0dd1f702348183698cd74
satyanarayana <sathyayadav27@gmail.com>
2024-04-02 08:43:02 +0530

    macbook first comming

---
+++ lib/empty/extra.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Eca extends StatefulWidget {
+  const Eca({super.key});
+
+  @override
+  State<Eca> createState() => _EcaState();
+}
+
+class _EcaState extends State<Eca> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
